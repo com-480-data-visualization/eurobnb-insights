a65d8ab6176bf81604e61490799360151f078a1f
Harkeerat Singh Sawhney <harkeerat@gmail.com>
2025-04-17 23:06:19 +0200

    Updates

--- eurobnb-insight-app/src/pages/Paris.jsx
+++
@@ -1,8 +0,0 @@
-export default function Paris() {
-    return (
-      <div className="city-page">
-        <h1>Paris</h1>
-        <p>Content about Amsterdam goes here</p>
-      </div>
-    )
-  }
